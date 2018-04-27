dd7e00b08066008979c9ca0751bd6b8aac9f328c
zhangyc310 <26365685@qq.com>
2018-04-27 21:29:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-docker build -t caffeina/php-fpm-oci8:latest --squash --compress --force-rm -f Dockerfile .  && \
-[[ $1 == '--push' ]] && docker push caffeina/php-fpm-oci8:latest
+wget https://github.com/caffeinalab/php-fpm-oci8/raw/master/oracle/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+wget https://github.com/caffeinalab/php-fpm-oci8/raw/master/oracle/instantclient-basic-linux.x64-12.2.0.1.0.zip
+wget https://github.com/caffeinalab/php-fpm-oci8/raw/master/oracle/instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
+
+#oci8 安装
+
+unzip -o instantclient-basic-linux.x64-12.2.0.1.0.zip -d /opt/oracle
+unzip -o instantclient-sdk-linux.x64-12.2.0.1.0.zip -d /opt/oracle
+
+cat  > /etc/php.d/oci8.ini << EOF
+extension=oci8.so
+EOF
+
+# create symbolic links
+ln -s /opt/oracle/instantclient_12_2/sqlplus /usr/bin/sqlplus
+ln -s /opt/oracle/instantclient_12_2 /opt/oracle/instantclient
+ln -s /opt/oracle/instantclient_12_2/libclntsh.so.12.1 /opt/oracle/instantclient/libclntsh.so
+ln -s /opt/oracle/instantclient_12_2/libocci.so.12.1  /opt/oracle/instantclient/libocci.so
+# install oci8
+echo 'instantclient,/opt/oracle/instantclient' | sudo pecl install oci8
